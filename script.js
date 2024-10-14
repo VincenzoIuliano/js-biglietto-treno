48e3b4705b5fa18ed1ff4315db129e1a2abcb314
VincenzoIuliano <bubucane@outlook.it>
2024-10-14 15:27:27 +0200

    ADD: first prompt input

---
+++ script.js
@@ -0,0 +1,8 @@
+// DEVO CALCOLARE IL PREZZO DEL BIGLIETTO DEL TRENO IN BASE ALL'ETA ED AL NUMERO DI KM PERCORSI 
+
+// CHIEDO AL NOSTRO UTENTE DI INSERIRE KM PERCORSI ED ETA 
+
+let expectedKm = parseInt(prompt("Inserisci i Km che prevedi di percorrere nel tuo viaggio"))
+let yourAge = parseInt(prompt("Inserisci la tua Età"))
+
+console.log(expectedKm , yourAge)
