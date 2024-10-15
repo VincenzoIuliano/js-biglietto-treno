44585d5c3895960ca8920bd8dec913363c52df35
VincenzoIuliano <bubucane@outlook.it>
2024-10-15 16:26:26 +0200

    EDIT: add data validity check

--- script.js
+++ script.js
@@ -7,21 +7,33 @@ const yourAge = parseInt(prompt("Inserisci la tua Età"))
 
 console.log(expectedKm , yourAge)
 
-// CALCOLO IL PREZZO BASE DEL BIGLIETTO
 
-let basePrice = 0.21 * expectedKm
-console.log ('Il prezzo base del tuo viaggio è di €' + basePrice)
+// CONTROLLO CHE KM ED ETA SIANO VALIDI 
+
+const dataIsValid = !isNaN(expectedKm) && !isNaN(yourAge) // boolean
+const dataIsBiggerThanZero = expectedKm >= 0 && yourAge >= 0 // boolean
 
 // RIASSEGNO BASE PRICE, CALCOLANDO GLI SCONTI PER FASCIA D'ETA'
 
-if ( yourAge < 18) {
-    let discountTwenty = basePrice * (20 / 100)
-    basePrice = basePrice - discountTwenty
-} else if (yourAge>65) {
-    let discountFourty = basePrice * (40 / 100)
-    basePrice = basePrice - discountFourty
-} 
+if (dataIsValid && dataIsBiggerThanZero) {
+    // CALCOLO IL PREZZO BASE DEL BIGLIETTO
 
-// MOSTRO AL NOSTRO UTENTE QUALE PREZZO DOVRA' PAGARE IN BASE ALLA SCONTISTICA SPETTANTE
+    let basePrice = 0.21 * expectedKm
+    console.log ('Il prezzo base del tuo viaggio è di €' + basePrice)
 
-console.log ('Gentile passeggero, il prezzo finale del tuo biglietto è di €' + basePrice.toFixed(2) )
+    // CALCOLO GLI SCONTI 
+    
+    if ( yourAge < 18) {
+        let discountTwenty = basePrice * (20 / 100)
+        basePrice = basePrice - discountTwenty
+    } else if (yourAge>65) {
+        let discountFourty = basePrice * (40 / 100)
+        basePrice = basePrice - discountFourty
+    } 
+    
+    // MOSTRO AL NOSTRO UTENTE QUALE PREZZO DOVRA' PAGARE IN BASE ALLA SCONTISTICA SPETTANTE
+    
+    console.log ('Gentile passeggero, il prezzo finale del tuo biglietto è di €' + basePrice.toFixed(2) )
+} else {
+    console.log ('I dati inseriti non sono validi')
+}
